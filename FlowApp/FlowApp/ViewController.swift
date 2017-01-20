544088083f95bc396833ac288ad838f2461cfa7c
Parth Adroja <parth2cool2008@gmail.com>
2017-01-20 23:45:30 +0530

    Added PDF and Mail

--- FlowApp/FlowApp/ViewController.swift
+++ FlowApp/FlowApp/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import SwiftyJSON
+import MessageUI
+import SimplePDF
 
 class ViewController: UIViewController {
 
@@ -15,6 +17,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var IBbtnYes: UIButton!
     @IBOutlet weak var IBbtnNo: UIButton!
     
+    var arrSavedStateDict = [[String: String]]()
     var modesJson: JSON?
     var nextPoint: String? {
         didSet {
@@ -31,18 +34,18 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
 
     @IBAction func IBbtnYesTap(_ sender: UIButton) {
         if let toPoint = nextPoint {
             goToNextPointOnYes(toPoint: toPoint)
+            saveDataForPdf(key: IBlblQuestions.text!, value: "YES")
         }
     }
     
     @IBAction func IBbtnNoTap(_ sender: UIButton) {
         if let toPoint = nextPoint {
-             goToNextPointOnNo(toPoint: toPoint)
+            goToNextPointOnNo(toPoint: toPoint)
+            saveDataForPdf(key: IBlblQuestions.text!, value: "NO")
         }
     }
 }
@@ -63,7 +66,7 @@ extension ViewController {
 
 extension ViewController {
     
-    //Set Json Data to View
+    //Set Json Data to View and save data
     func setDataToView() {
         IBlblQuestions.text = modesJson!["Start"]["text"].stringValue
         nextPoint = modesJson!["Start"]["toPoint"].stringValue
@@ -71,12 +74,77 @@ extension ViewController {
     
     func goToNextPointOnYes(toPoint: String) {
         nextPoint = modesJson![toPoint]["Yes"]["toPoint"].stringValue
-        print(nextPoint!)
     }
     
     func goToNextPointOnNo(toPoint: String) {
         nextPoint = modesJson![toPoint]["No"]["toPoint"].stringValue
-        print(nextPoint!)
     }
     
+    func saveDataForPdf(key: String, value: String) {
+        let dataDict = [key: value]
+        arrSavedStateDict.append(dataDict)
+//        createPDFWithData(dataArr: arrSavedStateDict)
+    }
+}
+
+extension ViewController {
+
+    func createPDFWithData(dataArr: [[String : String]]) {
+        
+        let A4paperSize = CGSize(width: 595, height: 842)
+        let pdf = SimplePDF(pageSize: A4paperSize)
+        for text in dataArr {
+            for (key,value) in text {
+                let question = key
+                let answer = value
+                pdf.addText("\(question)")
+                pdf.addText(" \(answer)")
+            }
+        }
+        
+        if let docDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+            let fileName = "flow.pdf"
+            let documentsFileName = docDir + "/" + fileName
+            let pdfData = pdf.generatePDFdata()
+            do {
+                try pdfData.write(to: URL(fileURLWithPath: documentsFileName), options: .atomic)
+                print("\nThe generated pdf can be found at:")
+                print("\n\t\(documentsFileName)\n")
+            } catch {
+                print(error)
+            }
+            let mailComposeViewController = configuredMailComposeViewController()
+            if MFMailComposeViewController.canSendMail() {
+                self.present(mailComposeViewController, animated: true, completion: nil)
+            } else {
+                self.showSendMailErrorAlert()
+            }
+        }
+    }
+}
+
+extension ViewController: MFMailComposeViewControllerDelegate {
+    
+    func configuredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self
+        mailComposerVC.setToRecipients(["parth.adroja.sa@gmail.com"])
+        mailComposerVC.setSubject("Sending you an pdf file...")
+        mailComposerVC.setMessageBody("Sending e-mail in-app is not so bad!", isHTML: false)
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let pdfFileName = documentsPath.appending("/flow.pdf")
+        let fileData = NSData(contentsOfFile: pdfFileName)
+        mailComposerVC.addAttachmentData(fileData as! Data, mimeType: "application/pdf ", fileName: "flow")
+        
+        return mailComposerVC
+    }
+    
+    func showSendMailErrorAlert() {
+        let sendMailErrorAlert = UIAlertView(title: "Could Not Send Email", message: "Your device could not send e-mail.  Please check e-mail configuration and try again.", delegate: self, cancelButtonTitle: "OK")
+        sendMailErrorAlert.show()
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
 }
---
+++ FlowApp/Pods/Target Support Files/SimplePDF/SimplePDF-umbrella.h
@@ -0,0 +1,9 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "SimplePDF.h"
+
+FOUNDATION_EXPORT double SimplePDFVersionNumber;
+FOUNDATION_EXPORT const unsigned char SimplePDFVersionString[];
+
